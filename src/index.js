08ed52be212f9cbdb8fdb704bcb26242a1bc1b48
fabifatal <fabian.zuniga.p@gmail.com>
2023-06-23 02:01:27 -0400

    correcion index js 2

--- src/index.js
+++ src/index.js
@@ -7,4 +7,9 @@ import './css/style.css';
 
 
 
-ReactDOM.render(<App/>,  document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
